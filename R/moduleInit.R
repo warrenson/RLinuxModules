853ab7c165ee5663330a1a1898e9fb813436acf2
Roy Storey <kiwiroy@users.noreply.github.com>
2021-06-11 23:00:09 +1200

    tidy up, test files exist, and stop colour control characters

--- R/moduleInit.R
+++ R/moduleInit.R
@@ -10,12 +10,12 @@
 moduleInit <- function( version = '3.2.10',
                         modulesHome = '/local/genome/Modules/3.2.10'){
 
-  # Check if modulecmd exists in the
-  if(!file.exists( file.path(modulesHome,"bin/modulecmd") )){
-    stop(file.path(modulesHome,"bin/modulecmd")," missing!\n",
+  # Check if modulecmd exists in the bin directory of modulesHome
+  module_cmd <- file.path(modulesHome, "bin", "modulecmd")
+  if(!file.exists(module_cmd)){
+    stop(module_cmd, " missing!\n",
          "  Module environment init failed!" )
   }
-
 
   if( is.na(Sys.getenv('MODULE_VERSION', unset = NA)) ){
     Sys.setenv(MODULE_VERSION_STACK = version,
@@ -27,18 +27,26 @@ moduleInit <- function( version = '3.2.10',
   Sys.setenv(MODULESHOME=modulesHome)
 
   if( is.na(Sys.getenv('MODULEPATH', unset = NA)) ){
-    txt <- readLines(file.path(Sys.getenv('MODULESHOME'),"init/.modulespath"))
+    dot_modulespath <- file.path(modulesHome, "init", ".modulespath")
+    path <- ""
+    if (file.exists(dot_modulespath)) {
+      txt <- readLines(con = dot_modulespath)
 
-    # remove commented lines and trim leading and trailing whitespace
-    txt <- gsub("^\\s+|\\s+$", "", sub("#.*$","",txt))
-    # paste together
-    path <- paste(txt[txt != ""],collapse=":")
+      # remove commented lines and trim leading and trailing whitespace
+      txt <- gsub("^\\s+|\\s+$", "", sub("#.*$","", txt))
+      # paste together
+      path <- paste(txt[txt != ""],collapse=":")
+    } else {
+      # use default
+      path <- file.path(modulesHome, 'modulefiles')
+    }
     Sys.setenv( MODULEPATH = path )
   }
 
   if( is.na(Sys.getenv('LOADEDMODULES', unset = NA)) ){
     Sys.setenv( LOADEDMODULES = "" )
   }
+  Sys.setenv(MODULES_COLORS = "")
   # for {r, engine="bash"} in Rmarkdown/other bash subprocesses to use 'module' function
   if( shell_has_bash() ) {
     module_function <- bash_func_name()
