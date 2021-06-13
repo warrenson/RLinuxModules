75bdc29406a467c95910d6e12b7f4abccc3674f7
Roy Storey <kiwiroy@users.noreply.github.com>
2021-06-14 10:00:59 +1200

    style

--- tests/testthat/test-initialise.R
+++ tests/testthat/test-initialise.R
@@ -1,12 +1,14 @@
 context("RLinuxModules initialise")
 
-modulesHome <- file.path(getwd(), "mock")
-empty_env <- c(MODULE_VERSION = NA,
-               MODULEPATH     = NA,
-               LOADEDMODULES  = NA,
-               MODULEHOME     = NA,
-               'BASH_FUNC_module%%' = NA,
-               'BASH_FUNC_module()' = NA)
+modulesHome  <- file.path(getwd(), "mock")
+empty_env <- c(
+  MODULE_VERSION = NA,
+  MODULEPATH     = NA,
+  LOADEDMODULES  = NA,
+  MODULEHOME     = NA,
+  'BASH_FUNC_module%%' = NA,
+  'BASH_FUNC_module()' = NA
+)
 
 test_that("initialise environment", {
   withr::with_envvar(empty_env, {
@@ -17,7 +19,8 @@ test_that("initialise environment", {
     expect_equivalent(Sys.getenv("MODULE_VERSION", unset = NA), "3.2.10")
     expect_equivalent(Sys.getenv("MODULE_VERSION_STACK", unset = NA), "3.2.10")
 
-    expect_equivalent(Sys.getenv("MODULEPATH", unset = NA), "/usr/local/share/modules")
+    expect_equivalent(Sys.getenv("MODULEPATH", unset = NA),
+                      "/usr/local/share/modules")
 
     expect_equivalent(Sys.getenv("LOADEDMODULES", unset = NA), "")
     skip_if_not(shell_has_bash(), message = "shell is not bash")
@@ -30,8 +33,10 @@ test_that("version not overwritten", {
   version_set[["MODULE_VERSION"]] <- 2000
   withr::with_envvar(version_set, {
     moduleInit(modulesHome = modulesHome)
-    expect_equivalent(Sys.getenv("MODULE_VERSION", unset = NA), as.character(version_set[["MODULE_VERSION"]]))
-    expect_equivalent(Sys.getenv("MODULE_VERSION_STACK", unset = NA), as.character(version_set[["MODULE_VERSION"]]))
+    expect_equivalent(Sys.getenv("MODULE_VERSION", unset = NA),
+                      as.character(version_set[["MODULE_VERSION"]]))
+    expect_equivalent(Sys.getenv("MODULE_VERSION_STACK", unset = NA),
+                      as.character(version_set[["MODULE_VERSION"]]))
   })
 })
 
@@ -46,7 +51,8 @@ test_that("loaded modules not overwritten", {
 
 test_that("loaded modules not overwritten", {
   modpath_set <- empty_env
-  modpath_set[["MODULEPATH"]] <- "/usr/share/Modules/modulefiles:/opt/share/modulefiles"
+  modpath_set[["MODULEPATH"]] <-
+    "/usr/share/Modules/modulefiles:/opt/share/modulefiles"
   withr::with_envvar(modpath_set, {
     moduleInit(modulesHome = modulesHome)
     expect_equivalent(Sys.getenv("MODULEPATH", unset = NA), modpath_set[["MODULEPATH"]])
@@ -54,12 +60,16 @@ test_that("loaded modules not overwritten", {
 })
 
 test_that("checking modulecmd works", {
-  withr::with_envvar(empty_env,
-      {
-        expect_error(moduleInit(modulesHome = "/opt/usr/local/unlikely/1.2.3"),
-                     paste("/opt/usr/local/unlikely/1.2.3/bin/modulecmd missing!",
-                           "  Module environment init failed!", sep = "\n"))
-      })
+  withr::with_envvar(empty_env, {
+    expect_error(
+      moduleInit(modulesHome = "/opt/usr/local/unlikely/1.2.3"),
+      paste(
+        "/opt/usr/local/unlikely/1.2.3/bin/modulecmd missing!",
+        "  Module environment init failed!",
+        sep = "\n"
+      )
+    )
+  })
 })
 
 
--- tests/testthat/test-lifecycle.R
+++ tests/testthat/test-lifecycle.R
@@ -13,58 +13,67 @@ empty_env[['LOADEDMODULES_modshare']] <- "git/2.10:1:perl/5.30:1"
 test_that("package lifecycle", {
   unloadNamespace("RLinuxModules")
 
-  withr::with_envvar(empty_env,
-                     {
-                       expect_true(is.na(version_set <- Sys.getenv("MODULE_VERSION", unset = NA)))
-                       expect_true(is.na(version_set <- Sys.getenv("MODULE_VERSION_STACK", unset = NA)))
-                       expect_true(requireNamespace("RLinuxModules"))
-                       expect_equivalent(
-                         names(RLinuxModules:::.rlinuxmodules),
-                         RLinuxModules:::.rlinuxmodules.names,
-                         label = "named list has names"
-                       )
+  withr::with_envvar(empty_env, {
+    expect_true(is.na(version_set <-
+                        Sys.getenv("MODULE_VERSION", unset = NA)))
+    expect_true(is.na(
+      version_set <- Sys.getenv("MODULE_VERSION_STACK", unset = NA)
+    ))
+    expect_true(requireNamespace("RLinuxModules"))
+    expect_equivalent(
+      names(RLinuxModules:::.rlinuxmodules),
+      RLinuxModules:::.rlinuxmodules.names,
+      label = "named list has names"
+    )
 
-                       set_vars <-
-                         RLinuxModules:::.rlinuxmodules[which(!is.na(RLinuxModules:::.rlinuxmodules))]
-                       expect_equivalent(set_vars,
-                                         c("git/2.10:perl/5.30", "git/2.10:1:perl/5.30:1"),
-                                         label = "loaded as per environment")
+    set_vars <-
+      RLinuxModules:::.rlinuxmodules[which(!is.na(RLinuxModules:::.rlinuxmodules))]
+    expect_equivalent(set_vars,
+                      c("git/2.10:perl/5.30", "git/2.10:1:perl/5.30:1"),
+                      label = "loaded as per environment")
 
-                       # initialise and test variables, redundant overall, but required for later tests
-                       withr::with_envvar(c("MODULE_VERSION"=NA, "MODULE_VERSION_STACK"=NA), {
-                         # TODO: resolve why
-                         # devtools::test() vs devtools::check()
-                         # without this protection, MODULE_VERSION and MODULE_VERSION_STACK interfere below
-                         moduleInit(modulesHome = modulesHome)
-                         # I think this protection resets the version vars on leaving this block so
-                         # post_init_vars does not include them.
-                         # Have a feeling devtools::test() is wrong rather than devtools::check()
-                       })
-                       post_init_vars <-
-                         Sys.getenv(RLinuxModules:::.rlinuxmodules.names, unset = NA)
-                       var_names      <-
-                         names(post_init_vars[which(!is.na(post_init_vars))])
-                       expect_equivalent(
-                         var_names,
-                         c(
-                           "LOADEDMODULES",
-                           "LOADEDMODULES_modshare",
-                           "MODULEPATH",
-                           "MODULESHOME",
-                           "MODULES_COLORS"
-                         ),
-                         label = "MODULE* variables set"
-                       )
+    # initialise and test variables, redundant overall, but required for later tests
+    withr::with_envvar(c(
+      "MODULE_VERSION" = NA,
+      "MODULE_VERSION_STACK" = NA
+    ), {
+      # TODO: resolve why
+      # devtools::test() vs devtools::check()
+      # without this protection, MODULE_VERSION and MODULE_VERSION_STACK interfere below
+      moduleInit(modulesHome = modulesHome, version = "4.4")
+      # I think this protection resets the version vars on leaving this block so
+      # post_init_vars does not include them.
+      # Have a feeling devtools::test() is wrong rather than devtools::check()
+      expect_equivalent(Sys.getenv("MODULE_VERSION", unset = NA), "4.4", label = "set")
+      expect_equivalent(Sys.getenv("MODULE_VERSION_STACK", unset = NA),
+                        "4.4",
+                        label = "also")
+    })
+    post_init_vars <-
+      Sys.getenv(RLinuxModules:::.rlinuxmodules.names, unset = NA)
+    var_names      <-
+      names(post_init_vars[which(!is.na(post_init_vars))])
+    expect_equivalent(
+      var_names,
+      c(
+        "LOADEDMODULES",
+        "LOADEDMODULES_modshare",
+        "MODULEPATH",
+        "MODULESHOME",
+        "MODULES_COLORS"
+      ),
+      label = "MODULE* variables set"
+    )
 
-                       # unload and test variables are reset
-                       unloadNamespace("RLinuxModules")
+    # unload and test variables are reset
+    unloadNamespace("RLinuxModules")
 
-                       post_unload_vars <-
-                         Sys.getenv(var_names, unset = NA)
-                       expect_equivalent(
-                         post_unload_vars,
-                         c("git/2.10:perl/5.30", "git/2.10:1:perl/5.30:1", NA, NA, NA),
-                         label = "reset"
-                       )
-                     })
+    post_unload_vars <-
+      Sys.getenv(var_names, unset = NA)
+    expect_equivalent(
+      post_unload_vars,
+      c("git/2.10:perl/5.30", "git/2.10:1:perl/5.30:1", NA, NA, NA),
+      label = "reset"
+    )
+  })
 })
